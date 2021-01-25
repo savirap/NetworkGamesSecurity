192da9f7e09b240ec021440855325843d9109acf
Savira Adilia <savira.adilia@gmail.com>
2021-01-25 18:24:59 +0700

    Unity Project

---
+++ NetworkGamesSecurity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int movementRate = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal") * Time.deltaTime * movementRate;
+        var verticalInput = Input.GetAxis("Vertical") * Time.deltaTime * movementRate;
+
+        transform.Translate(horizontalInput, verticalInput, 0);
+    }
+}
